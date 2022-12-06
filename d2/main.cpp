80019d1bcb6858fb598aeabeeb93be767e9c1c3b
Jacob Baumel <jacob.a.baumel@gmail.com>
2022-12-06 00:08:35 -0600

    Day 2

--- d2/main.cpp
+++ d2/main.cpp
@@ -1,9 +1,11 @@
 #include <fstream>
 #include <string>
+#include <iostream>
 
 using std::string;
 
 int getMatchPoints(char, char);
+int getMatchPoints2(char, char);
 
 int main() {
 		std::ifstream file("data.txt");
@@ -11,10 +13,32 @@ int main() {
 		int points = 0;
 
 		while(getline(file, line)) {
-				
+				int dpoints = getMatchPoints2(line[0], line[2]);
+				points += dpoints;
 		}
+
+		std::cout << "Total points: " << points << std::endl;
 }
 
 int getMatchPoints(char e, char p) {
+		int matchPoints = p - 'W';
+		if(p == e + 23) matchPoints+= 3;
+		else if(p == 'Z' && e == 'A') {}
+		else if(p > e + 23 || (p == 'X' && e == 'C')) matchPoints += 6;
 
+		return matchPoints;
+}
+
+int getMatchPoints2(char e, char p) {
+		if(p == 'Y') p = e + 23;
+		else if(p == 'X') {
+				p = e + 22;
+				if(p == 'W') p = 'Z';
+		}
+		else {
+				p = e + 24;
+				if(p == 'Z' + 1) p = 'X';
+		}
+
+		return getMatchPoints(e, p);
 }
